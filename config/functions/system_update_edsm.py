312867308177f5662e1b50c2a3df19ccffd57698
Simon AM <simon@agius-muscat.net>
2018-07-13 23:26:55 +0200

    Updated script to accept arguments and to batch requests

--- config/functions/system_update_edsm.py
+++ config/functions/system_update_edsm.py
@@ -1,6 +1,9 @@
+import argparse
 import json
 import requests
+import os
 import pymysql.cursors
+import time
 
 from datetime import datetime
 
@@ -10,30 +13,127 @@ pip3 install PyMySQL
 pip3 install requests
 """
 
-"""
-Edit these
-"""
-connection = pymysql.connect(host='localhost',
-                             port=3306,
-                             user='capiv2system',
-                             password='capiv2password',
-                             db='capiv2_207',
-                             charset='utf8',
-                             cursorclass=pymysql.cursors.DictCursor)
+config_file_path = '../environments/{}/database.json.example'
+# config_file_path = '../environments/{}/database.json'
 
-"""
-file_path = 'test.db'
-"""
-table_name = 'systems'
+# Initialising the Argument Parser and adding the three arguments required:
+# 1. Development
+# 2. Staging
+# 3. Production
+parser = argparse.ArgumentParser(description='Update script to retrieve missing stellar system information from EDSM')
+
+parser.add_argument(
+    '--development',
+    help='Run script in development mode',
+    action='store_true',
+    dest='is_dev'
+)
+
+parser.add_argument(
+    '--staging',
+    help='Run script in staging mode',
+    dest='is_staging',
+    action='store_true'
+)
+
+parser.add_argument(
+    '--production',
+    help='Run script in production mode',
+    dest='is_prod',
+    action='store_true'
+)
+
+# Add an argument to enable passing a non-default config file path
+parser.add_argument(
+    '-config-file-path',
+    help='Pass a specific config file path. If not provided, one according to the running mode (dev/stage/prod) will be used',
+    dest='config_path',
+    type=str,
+    default=config_file_path
+)
+
+# Add an argument to set a custom number of seconds waited between batches of updates
+parser.add_argument(
+    '-delay-seconds',
+    help='Customise number of seconds between update batches. Default is 1',
+    dest='delay_seconds',
+    type=int,
+    default=1
+)
+
+# Add an argument to set a maximum batch size of systems to be retrieved at once
+parser.add_argument(
+    '-batch-size',
+    help='Customise number of systems retrieved at once. Default is 25',
+    dest='batch_size',
+    type=int,
+    default=25
+)
+
+args = parser.parse_args()
+
+
+def get_status_string():
+    if args.is_prod:
+        return 'production'
+    if args.is_staging:
+        return 'staging'
+    if args.is_dev:
+        return 'development'
+    return None
+
+
+status_string = get_status_string()
+if not status_string:
+    print('Unable to run without specifying an environment type (pass --development, --staging or --production')
+    quit()
+
+actual_config_file_path = args.config_path.format(status_string)
+
+print('Reading in environment data from path: {}'.format(actual_config_file_path))
+file_exists = os.path.isfile(actual_config_file_path)
+if not file_exists:
+    print('Config file at path: {} does not exist. Please double check that the directory is correct'.format(actual_config_file_path))
+    quit()
+
+# Extract required config data
+with open(actual_config_file_path, 'r') as config_file:
+    data = json.loads(config_file.read())
+
+host = data['connections']['default']['settings']['host']
+port = data['connections']['default']['settings']['port']
+database = data['connections']['default']['settings']['database']
+username = data['connections']['default']['settings']['username']
+password = data['connections']['default']['settings']['password']
+
+# Initialise connection to MySQL
+connection = pymysql.connect(
+    host=host,
+    port=port,
+    user=username,
+    password=password,
+    db=database,
+    charset='utf8',
+    cursorclass=pymysql.cursors.DictCursor
+)
+
 day_difference_threshold = 1
 edsm_api_systems_url = 'https://www.edsm.net/api-v1/systems'
-
 current_date = datetime.now()
 names_to_update = []
 
-select_sql = 'SELECT systemName, updated_at FROM `{}`'.format(table_name)
+select_sql = 'SELECT systemName, updated_at FROM systems'
+
+
+# https://stackoverflow.com/questions/312443/how-do-you-split-a-list-into-evenly-sized-chunks
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
 
 try:
+    print('Opening connection to MySQL DB')
     with connection.cursor() as cursor:
         # Create a new record
         cursor.execute(select_sql)
@@ -45,33 +145,32 @@ try:
             elapsed_time = current_date - date
             if elapsed_time.days >= day_difference_threshold:
                 names_to_update.append(systemName)
-        print('Updating {} rows'.format(len(names_to_update)))
+            print('Updating {} rows'.format(len(names_to_update)))
         # add the params manually since they're duplicates
         url = edsm_api_systems_url + '?showId=1&showCoordinates=1&showPermit=1&'
-        for index, name in enumerate(names_to_update):
-            if not index == 0:
-                url += '&'
-            url += 'systemName[]={}'.format(name)
-        response = requests.get(url)
-        print(response.status_code)
-        all_systems_data = response.json()
-        print(all_systems_data)
-        with open('systemData.json', 'w') as file:
-            file.write(json.dumps(all_systems_data, indent=4, sort_keys=True))
-        for system_data in all_systems_data:
-            print(system_data)
-            insert_sql = 'UPDATE `{}` SET edsmCoordX=%s, edsmCoordY=%s, edsmCoordZ=%s, edsmID=%s, edsmID64=%s, edsmCoordLocked=%s WHERE systemName LIKE \'{}\''.format(table_name, system_data['name'])
-            cursor.execute(
-                insert_sql,
-                (
-                    system_data['coords']['x'],
-                    system_data['coords']['y'],
-                    system_data['coords']['z'],
-                    system_data['id'],
-                    system_data['id64'],
-                    1 if system_data['coordsLocked'] else 0
+        for chunk in list(chunks(names_to_update, args.batch_size)):
+            for index, name in enumerate(chunk):
+                if not index == 0:
+                    url += '&'
+                url += 'systemName[]={}'.format(name)
+            response = requests.get(url)
+            all_systems_data = response.json()
+            for system_data in all_systems_data:
+                print('Updating: {}'.format(system_data['name']))
+                insert_sql = 'UPDATE `systems` SET edsmCoordX=%s, edsmCoordY=%s, edsmCoordZ=%s, edsmID=%s, edsmID64=%s, edsmCoordLocked=%s WHERE systemName LIKE \'{}\''.format(system_data['name'])
+                cursor.execute(
+                    insert_sql,
+                    (
+                        system_data['coords']['x'],
+                        system_data['coords']['y'],
+                        system_data['coords']['z'],
+                        system_data['id'],
+                        system_data['id64'],
+                        1 if system_data['coordsLocked'] else 0
+                    )
                 )
-            )
+            time.sleep(args.delay_seconds)
     connection.commit()
 finally:
+    print('Closing connection to MySQL DB')
     connection.close()
