01f209bafc4ff67796e9639990af69bb2b8fd7e7
DMehaffy <derrickmehaffy@gmail.com>
2018-09-12 06:54:50 -0700

    Add GR/GS Active Obelisk Seed Files
    Removed script because it was crap, also fixed a few bugs.

--- scripts/downloadExampleData.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-csv_file="$PWD/exampleFilesURL.csv"
-cd $PWD && cd ../seed_files/CSV/exampleData/
-while IFS=, read -r newName url; do
-  wget –no-check-certificate -b -N -q -O "${PWD}/${newName}.csv" ''"${url}"''
-done <"$csv_file"
