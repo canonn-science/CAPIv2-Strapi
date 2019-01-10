cba461d83a618e815a140d41530d975db16eb901
DMehaffy <derrickmehaffy@gmail.com>
2019-01-10 05:26:52 -0700

    Updates per codex changes

---
+++ api/twtype/controllers/Twtype.js
@@ -0,0 +1,104 @@
+'use strict';
+
+/**
+ * Twtype.js controller
+ *
+ * @description: A set of functions called "actions" for managing `Twtype`.
+ */
+
+module.exports = {
+
+  /**
+   * Retrieve twtype records.
+   *
+   * @return {Object|Array}
+   */
+
+  find: async (ctx) => {
+    if (ctx.query._q) {
+      return strapi.services.twtype.search(ctx.query);
+    } else {
+      return strapi.services.twtype.fetchAll(ctx.query);
+    }
+  },
+
+  /**
+   * Retrieve a twtype record.
+   *
+   * @return {Object}
+   */
+
+  findOne: async (ctx) => {
+    return strapi.services.twtype.fetch(ctx.params);
+  },
+
+  /**
+   * Count twtype records.
+   *
+   * @return {Number}
+   */
+
+  count: async (ctx) => {
+    return strapi.services.twtype.count(ctx.query);
+  },
+
+  /**
+   * Create a/an twtype record.
+   *
+   * @return {Object}
+   */
+
+  create: async (ctx) => {
+    return strapi.services.twtype.add(ctx.request.body);
+  },
+
+  /**
+   * Update a/an twtype record.
+   *
+   * @return {Object}
+   */
+
+  update: async (ctx, next) => {
+    return strapi.services.twtype.edit(ctx.params, ctx.request.body) ;
+  },
+
+  /**
+   * Destroy a/an twtype record.
+   *
+   * @return {Object}
+   */
+
+  destroy: async (ctx, next) => {
+    return strapi.services.twtype.remove(ctx.params);
+  },
+
+  /**
+   * Add relation to a/an twtype record.
+   *
+   * @return {Object}
+   */
+
+  createRelation: async (ctx, next) => {
+    return strapi.services.twtype.addRelation(ctx.params, ctx.request.body);
+  },
+
+  /**
+   * Update relation to a/an twtype record.
+   *
+   * @return {Object}
+   */
+
+  updateRelation: async (ctx, next) => {
+    return strapi.services.twtype.editRelation(ctx.params, ctx.request.body);
+  },
+
+  /**
+   * Destroy relation to a/an twtype record.
+   *
+   * @return {Object}
+   */
+
+  destroyRelation: async (ctx, next) => {
+    return strapi.services.twtype.removeRelation(ctx.params, ctx.request.body);
+  }
+};
