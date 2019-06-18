7c3befecd61180b27f02475f29194f61cd396771
Derrick Mehaffy <derrickmehaffy@gmail.com>
2019-06-18 09:16:38 -0700

    remove dotenv loading in server.js

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 #!/usr/bin/env node
 'use strict';
 
-// Load environment variables
-//require('dotenv').config({ path: require('find-config')('.env') });
-
 // Start Strapi
 const strapi = require('strapi');
 strapi().start();
