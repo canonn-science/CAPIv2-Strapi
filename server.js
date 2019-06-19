8ba2accd0d9e08d0895205f26e03d9acf0ffb67e
Derrick Mehaffy <derrickmehaffy@gmail.com>
2019-06-18 23:05:01 -0700

    Readd server.js dotenv loading as PR fix didn't work

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 'use strict';
 
+// Load environment variables
+require('dotenv').config({ path: require('find-config')('.env') });
+
 // Start Strapi
 const strapi = require('strapi');
 strapi().start();
