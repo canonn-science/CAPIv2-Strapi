67027c8e387c63244fb3b4c2c67d29c0b05919c6
Derrick Mehaffy <derrickmehaffy@gmail.com>
2019-08-21 00:40:55 -0700

    fix tb reports and assign default values to subtype and cycle if empty

--- api/tbsite/services/Tbsite.js
+++ api/tbsite/services/Tbsite.js
@@ -5,4 +5,24 @@
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+  /**
+   * Promise to add an tbsite record
+   *
+   * @return {Promise}
+   */
+
+  create: async (values) => {
+
+    if (values.subtype == undefined) {
+      values.subtype = 1;
+    }
+
+    if (values.cycle == undefined) {
+      values.cycle = 1;
+    }
+
+    // If checks pass, proceed to create data
+    return strapi.query('Tbsite').create(values);
+  },
+};
