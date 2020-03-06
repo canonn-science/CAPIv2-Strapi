2cbdd5d73709576b02c78b3f728f783f5ba48239
Derrick Mehaffy <derrickmehaffy@gmail.com>
2020-03-06 07:40:50 -0700

    Move tbsubtype to policy to handle older clients

--- api/tbreport/controllers/Tbreport.js
+++ api/tbreport/controllers/Tbreport.js
@@ -1,29 +1,3 @@
 'use strict';
 
-const { sanitizeEntity } = require('strapi-utils');
-
-/**
- * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/guides/controllers.html#core-controllers)
- * to customize this controller
- */
-
-module.exports = {
-  /**
-   * Create a record.
-   *
-   * @return {Object}
-   */
-
-  create: async ctx => {
-    let entity;
-    let data = ctx.request.body;
-
-    // Need to convert this to a policy
-    if (!data.subtype) {
-      data.subtype = 'Unknown';
-    }
-
-    entity = await strapi.services.tbreport.create(data);
-    return sanitizeEntity(entity, { model: strapi.models.tbreport });
-  }
-};
+module.exports = {};
