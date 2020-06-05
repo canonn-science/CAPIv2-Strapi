f9aa9aa6f0611b6ae53c2c4a5f0ff94b29b05acd
Derrick Mehaffy <derrickmehaffy@gmail.com>
2020-06-05 10:03:50 -0700

    Remove Content-Range policy and create custom middleware
    Uses a middleware to set Content-Range in order to inject header even if entry is pulled from cache

--- middlewares/range/index.js
+++ middlewares/range/index.js
@@ -1,9 +1,46 @@
+const pluralize = require('pluralize');
+
 module.exports = strapi => {
   return {
     initialize() {
       strapi.app.use(async (ctx, next) => {
         await next();
-        console.log(ctx);
+
+        // Breakdown the Koa ctx.request.url to get plural model
+        let args = ctx.request.url.split(/\/|\?/);
+
+        // Convert plural to singular
+        let model = pluralize.singular(args[1]);
+
+        // Only match find queries not findOne/count or any other method
+        if(
+          ctx.request.method === 'GET' &&
+          ctx.params['id'] === undefined &&
+          ctx.params['id'] !== 'count' &&
+          model in strapi.models
+        ){
+          if (ctx.request.query._q) {
+            // Do countSearch and set header
+            try {
+              ctx.set(
+                'Content-Range',
+                await strapi.query(model).countSearch(ctx.request.query)
+              );
+            } catch (error) {
+              throw new Error('Failed to set countSearch header on model:' + model);
+            }
+          } else {
+            // Do count and set header
+            try {
+              ctx.set(
+                'Content-Range',
+                await strapi.query(model).count(ctx.request.query)
+              );
+            } catch (error) {
+              throw new Error('Failed to set count header on model:' + model);
+            }
+          }
+        }
       });
     },
   };
