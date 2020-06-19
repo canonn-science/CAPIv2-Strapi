0e3cc34dc01a4b99e8ed0e169620954e9d534c2b
Derrick Mehaffy <derrickmehaffy@gmail.com>
2020-06-19 03:24:35 -0700

    Add simple bulk material delete

--- api/materialreport/controllers/Materialreport.js
+++ api/materialreport/controllers/Materialreport.js
@@ -1,3 +1,28 @@
 'use strict';
 
-module.exports = {};
+module.exports = {
+  async deleteOldBulk(ctx) {
+    if (!ctx.query.month) {
+      return ctx.badRequest('Missing month parameter');
+    } else if (ctx.query.month < 1) {
+      return ctx.badRequest('Minimum month parameter is 1');
+    } else if ( ctx.query.month > 6) {
+      return ctx.badRequest('Maximum month parameter is 6');
+    }
+
+    let month = parseInt(ctx.query.month);
+
+    if (isNaN(month) === false) {
+      const result = await strapi.connections.default.raw(
+        `DELETE FROM materialreports WHERE created_at < (NOW() - INTERVAL ${month} MONTH)`
+      );
+
+      return {
+        intervalMonth: month,
+        deletedRecords: result[0].affectedRows
+      };
+    } else {
+      return ctx.badRequest('Your month parameter is not an integer between 1 and 6');
+    }
+  }
+};
