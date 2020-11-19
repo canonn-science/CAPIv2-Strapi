5f5b78f8e04b387ce5e15906850b1768a1a0eb01
DMehaffy <derrickmehaffy@gmail.com>
2020-11-18 18:26:43 -0800

    Update to Strapi v3.3.x and add EE License (#181)
    * Update to Strapi v3.2.4 and add EE License
    
    Signed-off-by: Derrick Mehaffy <derrickmehaffy@gmail.com>
    
    * WIP new admin roles and perms
    
    * Create user plugin settings model
    
    oneToOne relation with user, used to store EDMC plugin settings
    
    * Revert "Create user plugin settings model"
    
    This reverts commit 91b842a79944f4e0f29b687b0e84724ccd9566c5.
    
    * more cleanup of bad old data
    
    * more massive cleanup
    
    * Finish permissions and update to Strapi v3.2.5
    
    * prepare to write full migration script
    
    * Update Strapi to v3.3.3
    
    Signed-off-by: Derrick Mehaffy <derrickmehaffy@gmail.com>
    
    * Remove Strapi updater log
    
    * cleanup and dump core_store
    
    Updated views, permissions, and general settings => dump to file
    
    * Get scripts ready for migration
    
    * Update readme

--- scripts/SQL_Migration/create_test_databases.sql
+++
@@ -1,8 +0,0 @@
-drop database if exists capiv2_base;
-drop database if exists capiv2_example;
-drop database if exists capiv2_exampleNoData;
-drop database if exists capiv2_exampleWithData;
-create database capiv2_base CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-create database capiv2_example CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-create database capiv2_exampleNoData CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-create database capiv2_exampleWithData CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
